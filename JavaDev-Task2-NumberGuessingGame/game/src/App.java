d938f2f37f139923f361980ee82fb99addf00bae
Hadia Arain <hadiaarain667@gmail.com>
2026-07-17 03:25:11 +0500

    Fix method name and variable naming in App class

--- JavaDev-Task2-NumberGuessingGame/game/src/App.java
+++ JavaDev-Task2-NumberGuessingGame/game/src/App.java
@@ -1,6 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        game g = new game();
-        g.gamedisplay();
+    public static void main(String[] args) {
+        game game = new game();
+        game.gameDisplay();
     }
 }
